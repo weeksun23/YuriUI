aef8d69ba5e5abdcacc3531ecee25a500f50b06d
weeksun23 <jy013018@aliyun.com>
2014-09-23 17:10:35 +0800

    更新tree组件
    加入节点图标控制，根脉显示

--- src/js/avalon.tree.js
+++ src/js/avalon.tree.js
@@ -1,9 +1,45 @@
 define(["avalon"],function(avalon){
+	var nodeAttr = {
+		id : null,
+		iconCls : "",
+		openIconCls : "",
+		text : "",
+		loading : false,
+		state : null
+		//children : []
+	};
+	function eachNode(list){
+		for(var i=0,ii=list.length;i<ii;i++){
+			var item = list[i];
+			var ch = item.children;
+			if(!ch){
+				ch = item.children = [];
+			}
+			for(var j in nodeAttr){
+				if(item[j] === undefined){
+					item[j] = nodeAttr[j];
+				}
+			}
+			if(ch.length > 0){
+				eachNode(ch);
+			}
+		}
+	}
 	function getTreeStr(HTML_OR_TPL){
-		return "<li ms-repeat='"+HTML_OR_TPL+"'>" + 
-			"<i ms-if='el.state!==undefined' ms-class='tree-bg' ms-class-2='tree-collapsed:el.state===\"closed\"' ms-class-1='tree-expanded:el.state===\"open\"' ms-click='$toggleOpenExpand(el)'></i>" +
-			"<i ms-class='{{el.iconCls}}'></i><span class='tree-title'>{{el.text}}</span>" +
-			"<ul ms-if='el.children&&el.children.length' ms-visible='el.state===\"open\"' ms-include-src='\"TREE_TPL\"'></ul>"
+		return "<li ms-repeat='"+HTML_OR_TPL+"' ms-class-1='tree-node' ms-class='tree-node-last:$last'>" + 
+			"<span>" +
+				"<i ms-class='tree-bg:el.state' ms-class-2='tree-collapsed:el.state===\"closed\"' ms-class-1='tree-expanded:el.state===\"open\"' ms-class-3='tree-indent' ms-class-4='tree-bg:line' ms-class-5='tree-join:line && !$last' ms-class-6='tree-joinbottom:line && $last' ms-click='$toggleOpenExpand(el)'></i>" +
+				"<i ms-if='icon && el.iconCls !== false' " +
+					"ms-class='{{el.iconCls}}:el.iconCls && (!el.state || el.state === \"closed\" || (el.state === \"open\" && !el.openIconCls))' "+
+					"ms-class-1='{{el.openIconCls}}:el.openIconCls && el.state === \"open\"' " +
+					"ms-class-3='tree-indent tree-bg:!el.iconCls' " +
+					"ms-class-2='tree-folder:!el.iconCls && el.state===\"closed\"' " +
+					"ms-class-4='tree-folder-open:!el.iconCls && el.state===\"open\"' " +
+					"ms-class-5='tree-file:!el.state && !el.iconCls' " +
+					"ms-class-6='tree-icon-loading:el.loading'></i>" +
+				"<span class='tree-title'>{{el.text}}</span>" +
+			"</span>" +
+			"<ul ms-if='el.children&&el.children.length' ms-visible='el.state===\"open\"' ms-include-src='\"TREE_TPL\"'></ul>" +
 		"</li>";
 	}
 	var widget = avalon.ui.tree = function(element, data, vmodels){
@@ -12,12 +48,14 @@ define(["avalon"],function(avalon){
 		}
 		var vmodel = avalon.define(data.treeId,function(vm){
 			var options = data.treeOptions;
+			eachNode(options.treeList);
 			avalon.mix(vm, options);
 			vm.widgetElement = element;
 			vm.template = getTreeStr("treeList");
 			vm.$init = function(){
 				var $el = avalon(element);
 				$el.addClass('tree');
+				vmodel.line && $el.addClass("tree-line");
 				element.innerHTML = vmodel.template;
 				avalon.scan(element, vmodel);
 			};
@@ -25,28 +63,26 @@ define(["avalon"],function(avalon){
 				element.innerHTML = element.textContent = "";
 			};
 			vm.$toggleOpenExpand = function(el){
-				var cls = el.iconCls;
-				if(cls.indexOf('tree-icon-loading') !== -1) return;
+				if(!el.state) return;
+				if(el.loading) return;
 				if(el.state === 'closed'){
 					if(el.children && el.children.length){
 						el.state = 'open';
 					}else{
-						el.iconCls = cls + " tree-icon-loading";
 						var me = this;
+						el.loading = true;
 						setTimeout(function(){
-							el.iconCls = cls;
+							el.loading = false;
 							el.state = 'open';
-							el.children = [{
+							var ch = [{
 								text : "children",
-								iconCls : "icon-reload",
-								state : "closed",
-								children : []
+								state : "closed"
 							},{
 								text : "children",
-								iconCls : "icon-reload",
-								state : "closed",
-								children : []
+								state : "closed"
 							}];
+							eachNode(ch);
+							el.children = ch;
 						},1000);
 					}
 				}else{
@@ -57,6 +93,8 @@ define(["avalon"],function(avalon){
 		return vmodel;
 	};
 	widget.defaults = {
-		treeList : []
+		treeList : [],
+		line : false,
+		icon : true
 	};
 });
