4e7273938141309c2754666e7576d112dd1f60c0
weeksun23 <jy013018@aliyun.com>
2014-09-29 21:02:18 +0800

    增加onSelect方法
    增加onSelect方法

--- src/tab/avalon.tab.js
+++ src/tab/avalon.tab.js
@@ -1,15 +1,5 @@
 define(["avalon","text!./avalon.tab.html"],function(avalon,templete){
-	/*
-	tab : {
-		title : 'aaaa',
-		closeable : true,
-		iconCls : "icon-save",
-		href : 'javascript:void(0)'
-	}
-	panel : {
-		content : "<div>hello avalon</div>"	
-	}
-	*/
+	//扫描dom 获取配置数据
 	function getData(el,tagName,isHeader){
 		var children = el.childNodes;
 		var result = [];
@@ -37,20 +27,64 @@ define(["avalon","text!./avalon.tab.html"],function(avalon,templete){
 			result : result
 		};
 	}
+	//初始化监控属性
+	function initTabData(target,data){
+		avalon.each(data,function(i,item){
+			for(var j in target){
+				if(item[j] === undefined){
+					item[j] = target[j];
+				}
+			}
+		});
+	}
+	//点击标题事件处理
 	function doClickTab(vmodel,target){
 		var i = target.getAttribute("data-index");
 		vmodel.curIndex = i;
+		var ii = Number(i);
+		var el = vmodel.tabData[ii];
+		vmodel.onSelect(el.$init,ii);
+		el.$init = true;
+	}
+	//删除tab
+	function doCloseTab(vmodel,index){
+		if(vmodel.curIndex === index + ""){
+			if(index > 0){
+				vmodel.curIndex = String(index - 1);
+			}
+		}
+		vmodel.tabData.removeAt(index);
+		vmodel.panelData.removeAt(index);
 	}
 	var widget = avalon.ui.tab = function(element, data, vmodels){
 		var options = data.tabOptions;
-		var tabResult = options.tabData || getData(element.getElementsByTagName("ul")[0],"li",true);
-		options.tabData = tabResult.result;
-		options.curIndex = tabResult.curIndex ? tabResult.curIndex : options.curIndex;
-		options.panelData = options.panelData || getData(element.getElementsByTagName("div")[0],"div",false).result;
+		if(options.tabData){
+			initTabData({
+				title : '',
+				closeable : false,
+				iconCls : null,
+				href : 'javascript:void(0)'
+			},options.tabData);
+		}else{
+			var tabResult = getData(element.getElementsByTagName("ul")[0],"li",true);
+			options.tabData = tabResult.result;
+			options.curIndex = tabResult.curIndex ? tabResult.curIndex : options.curIndex;
+		}
+		if(options.panelData){
+			initTabData({
+				content : ""
+			},options.panelData);
+		}else{
+			options.panelData = getData(element.getElementsByTagName("div")[0],"div",false).result;
+		}
 		var vmodel = avalon.define(data.tabId,function(vm){
 			avalon.mix(vm, options);
-			//vm.$skipArray = [];
+			vm.$skipArray = ["border","fit","onSelect"];
 			vm.$init = function(){
+				var $el = avalon(element);
+				$el.attr("ms-css-width",vmodel.width);
+				vmodel.fit && $el.addClass("tab-fit");
+				vmodel.border && $el.addClass("tab-border");
 				element.innerHTML = templete;
 				avalon.scan(element,vmodel);
 			};
@@ -59,8 +93,13 @@ define(["avalon","text!./avalon.tab.html"],function(avalon,templete){
 			};
 			vm.$clickTab = function(e){
 				var target = e.target;
-				avalon.log(target);
-				if(target.getAttribute("data-type") !== "tabLink"){
+				var type = target.getAttribute("data-type");
+				if(type === "tabLink"){
+					doClickTab(vmodel,target);
+				}else if(type === "tabClose"){
+					e.stopPropagation();
+					doCloseTab(vmodel,Number(target.parentNode.getAttribute("data-index")));
+				}else{
 					var pNode = target.parentNode;
 					while(pNode.tagName.toLowerCase() !== 'body'){
 						if(pNode.getAttribute("data-type") === "tabLink"){
@@ -69,8 +108,6 @@ define(["avalon","text!./avalon.tab.html"],function(avalon,templete){
 						}
 						pNode = pNode.parentNode;
 					}
-				}else{
-					doClickTab(vmodel,target);
 				}
 			};
 		});
@@ -79,6 +116,11 @@ define(["avalon","text!./avalon.tab.html"],function(avalon,templete){
 	widget.defaults = {
 		tabData : null,
 		panelData : null,
-		curIndex : '3'
+		curIndex : '0',
+		border : true,
+		height : null,
+		width : null,
+		fit : false,
+		onSelect : avalon.noop
 	};
 });
